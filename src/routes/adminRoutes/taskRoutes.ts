08a0e102983aa0e352f5ccdeda940398829d85b5
codevoid048 <williamkeri007@gmail.com>
2025-09-24 11:44:50 +0000

    merge from sai

--- src/routes/adminRoutes/taskRoutes.ts
+++ src/routes/adminRoutes/taskRoutes.ts
@@ -5,21 +5,12 @@ import {
   getTaskById,
   updateTask,
   deleteTask,
-<<<<<<< HEAD:src/routes/taskRoutes.ts
-} from "../controllers/taskControllers.js";
-import { authenticate,authorizeRole} from "../middlewares/authMiddleware.ts.js";
-
-const router = Router();
-
-router.use(authenticate,authorizeRole("admin")); //if give access permission to team by authorizeRole("team")
-=======
 } from "../../controllers/adminControllers/taskControllers.js";
 import { authenticateAdmin } from "../../middlewares/authenticateAdmin.js";
 
 const router = Router();
 
 router.use(authenticateAdmin);
->>>>>>> 112abe90705820b6201af877b55839fa3ece537e:src/routes/adminRoutes/taskRoutes.ts
 
 // CRUD Endpoints
 router.post("/", createTask); // Create
