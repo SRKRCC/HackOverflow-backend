2da629b9097559443d53114e60ac041f4cc484c0
codevoid048 <williamkeri007@gmail.com>
2025-11-01 12:39:55 +0530

    cors fixed

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://hackoverflow.srkrcodingclub.in",
   credentials: true,                          
 }));
 
